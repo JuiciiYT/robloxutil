9a0442049e5022f4500b6c40ca3e64cbfdf8e7d8
JuiciiYT <56958664+JuiciiYT@users.noreply.github.com>
2021-01-06 18:22:12 +0000

    Minify, Comments and Commands

--- index.js
+++ index.js
@@ -1,87 +1,32 @@
-const { app, BrowserWindow, TouchBar, shell, Menu, MenuItem }                  = require('electron');
-const { TouchBarLabel, TouchBarButton, TouchBarSpacer } = TouchBar
-const prompt                                            = require('electron-prompt-redux');
-const fs                                                = require('fs')
+/*
 
-const menu = new Menu()
-menu.append(new MenuItem({
-  label: 'RobloxUtil',
-  submenu: [{
-    role: 'help',
-    accelerator: process.platform === 'darwin' ? 'Cmd+H' : 'Ctrl+H',
-    click: () => { shell.openExternal("https://github.com/RobloxUtil/robloxutil/issues") }
-  }]
-}))
+   ___       __   __         __  ____  _ __
+  / _ \___  / /  / /__ __ __/ / / / /_(_) /
+ / , _/ _ \/ _ \/ / _ \\ \ / /_/ / __/ / / 
+/_/|_|\___/_.__/_/\___/_\_\\____/\__/_/_/  
+_________________________________________
 
-menu.append(new MenuItem({
-  label: 'Edit',
-  submenu: [{
-    role: 'copy',
-    accelerator: process.platform === 'darwin' ? 'Cmd+C' : 'Ctrl+C'
-  }]
-}))
-menu.append(new MenuItem({ label: 'Misc', submenu: [{ role: 'reload', accelerator: process.platform === 'darwin' ? 'Cmd+R' : 'Ctrl+R' }] }))
+     Developed by JuiciiOfficial 
+
+*/
 
 
-Menu.setApplicationMenu(menu)
 
-function createWindow () {
-  prompt({
-    title: 'Enter a Roblox Username',
-    label: 'Username:',
-    value: '',
-    inputAttrs: {
-        type: 'text'
-    },
-    type: 'input'
-})
-.then((r) => {
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        titleBarStyle: 'hiddenInset',
-        webPreferences: {
-          nodeIntegration: true
-        }
-      })
-    win.loadFile('loading.html')
-    if(r === null) {
-        console.log('user cancelled');
-        process.exit()
-    } else {
-        (async() => {
-            const getUser = require("roblox-user-information")
-            await getUser(r).then(user => {
-                const username                                          = new TouchBarLabel({ label: 'Username: ' + r })
-                const issue                                             = new TouchBarButton({ 
-                    label: 'Report an issue', 
-                    backgroundColor: '#FF0000',
-                    click: () => {
-                        shell.openExternal(`mailto:robloxutil@gmail.com?subject=[ISSUE] Roblox User Grabber&body=%5BError%20Info%5D%0A%0ARoblox%20Username%3A%20${r}%0AError%20Type%3A%202%0ADevice%3A%20MacOS%0A%0A%5BUser%20Info%5D%0A%0A`)
-                      }
-                })
-                const touchBar                                          = new TouchBar({ items: [ username, new TouchBarSpacer({ size: 'small' }), issue ] })
-                fs.writeFile('./result.json', `{ "input": "${r}" }`, (err) => {
-                    // throws an error, you could also catch it here
-                    if (err) throw err;
-                    
-                })
-                fs.writeFile('./user.json', JSON.stringify(user), (err) => {
-                    // throws an error, you could also catch it here
-                    if (err) throw err;
-                    win.loadFile("done.html")
-                })
-                setTimeout(function(){ win.loadFile('index.html'); win.setTouchBar(touchBar) }, 5000);
-            })
-          })()
-        
-    }
-})
-.catch(console.error);
-  
-}
+// VARIABLES
 
-app.whenReady().then(createWindow)
+const { app, BrowserWindow, TouchBar, shell, Menu, MenuItem, globalShortcut }                  = require('electron');
+const { TouchBarLabel, TouchBarButton, TouchBarSpacer }                                        = TouchBar
+const prompt                                                                                   = require('electron-prompt-redux');
+const fs                                                                                       = require('fs')
+const menu                                                                                     = new Menu()
+const getUser                                                                                  = require("roblox-user-information")
+
+// APP FUNCTIONS
+app.whenReady().then(() => {
+  globalShortcut.register('CommandOrControl+Y', () => {
+    shell.openExternal("https://web.roblox.com/search/users")
+  })
+}).then(createWindow)
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
@@ -94,4 +39,95 @@ app.on('activate', () => {
     createWindow()
   }
 })
+
+// MENU ITEMS and COMMANDS
+menu.append(new MenuItem({
+  label: 'RobloxUtil',
+    submenu: [
+      { 
+        role: 'help', 
+        accelerator: process.platform === 'darwin' ? 'Cmd+H' : 'Ctrl+H', 
+        click: () => { shell.openExternal("https://github.com/RobloxUtil/robloxutil/issues") }
+      },  
+    ]  
+}))
+
+menu.append(new MenuItem({
+  label: 'Edit',
+    submenu: [
+      {role: 'undo'},
+      {role: 'redo'},
+      {type: 'separator'},
+      {role: 'cut'},
+      {role: 'copy'},
+      {role: 'paste'},
+      {role: 'pasteandmatchstyle'},
+      {role: 'delete'},
+      {role: 'selectall'}
+    ]
+}))
+
+menu.append(new MenuItem({ 
+  label: 'Misc', 
+    submenu: [
+      { 
+        role: 'reload', 
+        accelerator: process.platform === 'darwin' ? 'Cmd+R' : 'Ctrl+R' 
+      }
+    ] 
+}))
+
+// ADD THE MENU
+Menu.setApplicationMenu(menu)
+
+// CREATE WINDOW FUNCTION
+function createWindow () {
+
+  // PROMPT FOR USERNAME
+  prompt({
+    title: 'Enter a Roblox Username',
+    label: 'Username:',
+    value: '',
+    inputAttrs: {
+        type: 'text'
+    },
+    type: 'input'
+  })
+  .then((r) => {
+    // VARIABLE FOR WINDOW
+    const win = new BrowserWindow({
+      width: 800,
+      height: 600,
+      titleBarStyle: 'hiddenInset',
+      webPreferences: {
+        nodeIntegration: true
+      }
+  })
+    
+    // LOAD THE LOADING SCREEN
+    win.loadFile('loading.html')
+
+    // IF NO INPUT OR CANCEL, HIDE
+    if(r === null) {
+      win.hide()
+    } else {
+
+      // GET ROBLOX USER DATA FROM VARIABLE 'r'
+      (async() => {
+         await getUser(r).then(user => {
+            // TOUCHBAR for MacOS
+            const username = new TouchBarLabel({ label: 'Username: ' + r })
+            const issue = new TouchBarButton({ label: 'Report an issue', backgroundColor: '#FF0000', click: () => { shell.openExternal(`mailto:robloxutil@gmail.com?subject=[ISSUE] Roblox User Grabber&body=%5BError%20Info%5D%0A%0ARoblox%20Username%3A%20${r}%0AError%20Type%3A%202%0ADevice%3A%20MacOS%0A%0A%5BUser%20Info%5D%0A%0A`)}})
+            const touchBar = new TouchBar({ items: [ username, new TouchBarSpacer({ size: 'small' }), issue ] })
+
+            // WRITE JSON FILES AND LOAD THE DONE SCREEN
+            fs.writeFile('./result.json', `{ "input": "${r}" }`, (err) => { if (err) throw err })
+            fs.writeFile('./user.json', JSON.stringify(user), (err) => { if (err) throw err; win.loadFile("done.html")})
+            setTimeout(function(){ win.loadFile('index.html'); win.setTouchBar(touchBar) }, 5000);
+        })
+      })()  
+    }
+ })
+ .catch(console.error);
+}
 
