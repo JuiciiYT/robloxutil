e7835361b0af6c6457fadc7fca516764566edeb4
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-10 18:21:44 +0000

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -5,7 +5,6 @@ if command git --version 2>&1 >/dev/null; then
      echo "\e[1m\e[32mRobloxUtil \e[0m\e[32mwas installed";
   else
      echo "\e[31m\e[1mNode.js \e[0m\e[31mis not installed.";
-   fi
 else
   echo "\e[31m\e[1mGit \e[0m\e[31mis not installed.";
 fi;
