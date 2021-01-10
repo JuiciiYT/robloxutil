59eda5bb97fd8dbc4eb9364b92d7ae254fdacff4
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-10 18:13:45 +0000

    Create install.sh

---
+++ install/install.sh
@@ -0,0 +1,13 @@
+git --version 2>&1 >/dev/null # improvement by tripleee
+GIT_IS_AVAILABLE=$?
+# ...
+if [ $GIT_IS_AVAILABLE -eq 0 ]; then 
+  if command --version node &>/dev/null; then
+     git clone https://github.com/RobloxUtil/robloxutil.git
+     npm install
+   else
+     echo "\e[31m\e[1mNode.js \e[0m\e[31mis not installed."
+   fu
+else
+  echo "\e[31m\e[1mGit \e[0m\e[31mis not installed."
+fi
