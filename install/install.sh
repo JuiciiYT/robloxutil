a5fc8295205fe1cfacd681f7b84396d8eb883172
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-10 18:24:55 +0000

    Update install.sh

--- install/install.sh
+++ install/install.sh
@@ -1,4 +1,3 @@
-
-     git clone https://github.com/RobloxUtil/robloxutil.git;
-     npm install;
-     echo "\e[1m\e[32mRobloxUtil \e[0m\e[32mwas installed";
+git clone https://github.com/RobloxUtil/robloxutil.git
+npm install
+echo "\e[1m\e[32mRobloxUtil \e[0m\e[32mwas installed"
