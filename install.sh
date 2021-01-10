b11e96c74ccc329d066ad636fb531700c0544e8c
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-10 19:16:11 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,18 @@
-git clone https://github.com/RobloxUtil/robloxutil.git
-cd robloxutil
-npm install
-command echo -e "\e[32m[ ✔ ]\e[m \e \e[1mRobloxUtil \e[0mwas successfully installed."
+BAR='##############################'
+FILL='------------------------------'
+totalLines=$(wc -l $file | awk '{print $1}')  # num. lines in file
+barLen=30
+
+# --- iterate over lines in csv file ---
+count=0
+while IFS=, read -r _ col1 col2 col3; do
+    # update progress bar
+    count=$(($count + 1))
+    percent=$((($count * 100 / $totalLines * 100) / 100))
+    i=$(($percent * $barLen / 100))
+    echo -ne "\r[${BAR:0:$i}${FILL:$i:barLen}] $count/$totalLines ($percent%)"
+
+    # other stuff
+    (...)
+done git clone https://github.com/RobloxUtil/robloxutil.git; cd robloxutil; npm install
+echo -e "\e[32m[ ✔ ]\e[m \e \e[1mRobloxUtil \e[0mwas successfully installed."
