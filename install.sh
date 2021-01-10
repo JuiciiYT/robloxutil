fce10f49552de033b200a0f835d6a70b4ee4b780
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-10 20:02:03 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,20 +1,28 @@
-spin[0]="[ - ] Installing..."
-spin[1]="[ \\ ] Installing..."
-spin[2]="[ | ] Installing..."
-spin[3]="[ / ] Installing..."
+count=0
+total=34
+start=`date +%s`
 
-while true; do
-  echo -e "${spin[0]}\033[K\r"
-  sleep 1
-  echo -e "${spin[1]}\033[K\r"
-  sleep 1
-  echo -e "${spin[2]}\033[K\r"
-  sleep 1
-  echo -e "${spin[3]}\033[K\r"
-  sleep 1
-done
-
+while [ $count -lt $total ]; do
 git clone https://github.com/RobloxUtil/robloxutil.git
 cd robloxutil
 npm install
-echo -ne "\e[32m[ ✔ ]\e[m \e \e[1mRobloxUtil \e[0mwas successfully installed."
+  sleep 0.5 # this is work
+  cur=`date +%s`
+  count=$(( $count + 1 ))
+  pd=$(( $count * 73 / $total ))
+  runtime=$(( $cur-$start ))
+  estremain=$(( ($runtime * $total / $count)-$runtime ))
+  printf "\r%d.%d%% complete ($count of $total) - est %d:%0.2d remaining\e[K" $(( $count*100/$total )) $(( ($count*1000/$total)%10)) $(( $estremain/60 )) $(( $estremain%60 ))
+done
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDR="\e[0m"
+BOLD="\e[1m"
+
+echo -e "${GREEN}[ ✔ ]${END}${BOLD}RobloxUtil${END} was installed successfully!"
+
+
+
+
+
