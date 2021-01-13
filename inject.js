989aeeec4e83da060a692f0acf2adce586aa7bf8
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-13 15:26:38 +0000

    Update inject.js

--- inject.js
+++ inject.js
@@ -1,3 +1,4 @@
+//Detect if Mac
 if (navigator.platform === 'MacIntel' || navigator.platform === 'MacPPC') {
     document.getElementById("macos").innerHTML = '<link rel="stylesheet" href="./mac.css" />'
 }
