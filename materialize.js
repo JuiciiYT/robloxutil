acbd4c9f964c71ac663396ad7c8970c9a8937cf7
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-13 15:24:14 +0000

    Update materialize.js

--- materialize.js
+++ materialize.js
@@ -1,14 +1,20 @@
+// Sidenav Init
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
 });
 
+// MaterialBox Init
+
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
-  });
+});
+
+// Modal Init
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.modal');
     var instances = M.Modal.init(elems);
-  });
+});
