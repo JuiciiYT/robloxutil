431c0a2f54600aefd47ce9755a48339f22d99e89
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-14 11:01:44 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,29 +1,3 @@
-count=0
-total=34
-start=`date +%s`
-
-git clone https://github.com/RobloxUtil/robloxutil.git &
-cd robloxutil &
-npm install &
-clear &
-while [ $count -lt $total ]; do
-  sleep 0.5 # this is work
-  cur=`date +%s`
-  count=$(( $count + 1 ))
-  pd=$(( $count * 73 / $total ))
-  runtime=$(( $cur-$start ))
-  estremain=$(( ($runtime * $total / $count)-$runtime ))
-  printf "\r%d.%d%% complete ($count of $total) - est %d:%0.2d remaining\e[K" $(( $count*100/$total )) $(( ($count*1000/$total)%10)) $(( $estremain/60 )) $(( $estremain%60 ))
-done
-
-RED="\e[31m"
-GREEN="\e[32m"
-ENDR="\e[0m"
-BOLD="\e[1m"
-
-echo -e "${GREEN}[ ✔ ]${END}${BOLD}RobloxUtil${END} was installed successfully!"
-
-
-
-
-
+echo "If this fails you probably don't have Node.js or git"
+git clone -b https://github.com/RobloxUtil/robloxutil.git && cd robloxutil
+npm i && echo "Installation Finished! You may now run the app."
