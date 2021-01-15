368e4aad67d095305d6a2c2fcb910e4c7ba98de8
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-01-15 15:09:23 +0000

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,4 @@
 echo "If this fails you probably don't have Node.js or git"
-git clone -b https://github.com/RobloxUtil/robloxutil.git && cd robloxutil
+git clone -b v1.0.0 https://github.com/RobloxUtil/robloxutil.git && cd robloxutil
 npm i && echo "Installation Finished! You may now run the app." && echo "Directory: ./robloxutil"
 echo "Run using npm start"
