8e786bdbd65a6873b24ece187ad78820bcb244ed
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-04-14 18:37:24 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,4 +1,8 @@
-echo "[⚠️ ] Git and Node.js must be installed"
-git clone -b v1.0.3 https://github.com/RBXUtil/RBXUtil.git && cd RBXUtil
-npm i && echo "[🎉] Installation Finished!"
-echo "[📝] Run the command 'cd RBXUtil' and 'npm start'"
+read -p "[⚠️ ] Git and Node.js must be installed. Enter 'Ok' to continue." -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY = Ok ]]
+then
+    git clone -b v1.0.3 https://github.com/RBXUtil/RBXUtil.git && cd RBXUtil
+    npm i && echo "[🎉] Installation Finished!"
+    echo "[📝] Run the command 'cd RBXUtil' and 'npm start'"
+fi
