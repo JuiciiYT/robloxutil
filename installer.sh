8e5238e17b82195d0a05aee9b901b5619d895eb9
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-04-14 19:58:57 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,9 +1,9 @@
 echo "[⚠️ ] Git and Node.js must be installed."
 echo "[🐱] Cloning Github Repo."
 sleep 2
-git clone -b v1.0.3 https://github.com/RBXUtil/RBXUtil.git > /dev/null
+git clone -b v1.0.3 https://github.com/RBXUtil/RBXUtil.git
 echo "[📦] Installing Node.js dependencies."
 sleep 3
-cd RBXUtil; npm i > /dev/null && sleep 2 && echo "[🎉] Installation Finished!"
+cd RBXUtil; npm i && sleep 2 && echo "[🎉] Installation Finished!"
 echo "[📝] Run the command 'cd RBXUtil' and 'npm start'"
 
