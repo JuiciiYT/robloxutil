e6efd541e2aac5d4b23cfe204f7d5f1d26a42117
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-04-14 18:51:03 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 read -p "[⚠️ ] Git and Node.js must be installed. Enter 'Ok' to continue." -n 1 -r
 
-if [[ $REPLY = Ok ]] then
+if [[ $REPLY = Ok ]]; then
     echo "[🐱] Cloning Github Repo."
     git clone -b v1.0.3 https://github.com/RBXUtil/RBXUtil.git > /dev/null && cd RBXUtil
     echo "[📦] Installing Node.js dependencies."
