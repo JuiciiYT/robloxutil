90d308c3628da55cd93f162de5ed89780fc45eb0
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-04-24 23:00:33 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,13 +1,2 @@
-@echo off
-Rem I will turn into a convertable... LETS GOOOOO!
-echo [ALERT] Git and Node.js must be installed.
-echo [INFO] Cloning Github Repo.
-timeout 2
 git clone -b v1.0.3 https://github.com/RBXUtil/RBXUtil.git
-echo [INFO] Installing Node.js dependencies.
-timeout 3
 cd RBXUtil && npm i
-timeout 2
-echo [INFO] Installation Finished!
-echo [INFO] Run the command 'cd RBXUtil' and 'npm start'
-
