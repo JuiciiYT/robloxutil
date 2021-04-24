a17a6f190161a56fd002922f4086a4f4e171b065
JuiciiOfficial <56958664+JuiciiYT@users.noreply.github.com>
2021-04-24 22:57:15 +0100

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,14 +1,12 @@
 @echo off
-echo "[⚠️ ] Git and Node.js must be installed."
-echo "[🐱] Cloning Github Repo."
+echo [ALERT] Git and Node.js must be installed.
+echo [INFO] Cloning Github Repo.
 timeout 2
 git clone -b v1.0.3 https://github.com/RBXUtil/RBXUtil.git
-echo "[📦] Installing Node.js dependencies."
+echo [INFO] Installing Node.js dependencies.
 timeout 3
 cd RBXUtil && npm i
 timeout 2
-echo "[🎉] Installation Finished!"
-set location=`pwd`
-echo "[📝] Installed in: $location"
-echo "[📝] Run the command 'cd RBXUtil' and 'npm start'"
+echo [INFO] Installation Finished!
+echo [INFO] Run the command 'cd RBXUtil' and 'npm start'
 
