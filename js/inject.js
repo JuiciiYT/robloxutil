0ee4b89c5d8777cbff5d3f6b79d01c829c67a49b
JuiciiYT <ayomideilesanmi10@gmail.com>
2021-04-25 09:13:48 +0100

    minor changes

--- js/inject.js
+++ js/inject.js
@@ -1,4 +1,4 @@
 //Detect if Mac
-if (process.platform = "darwin") {
+if (process.platform === "darwin") {
     document.getElementById("macos").innerHTML = '<link rel="stylesheet" href="./css/mac.css" />'
 }
